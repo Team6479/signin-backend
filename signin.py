7b453867efd48fc7316a50d8109f0fa37383a3f3
leo@leomwilson.com <lwilson@btw4.localdomain>
2019-11-13 15:40:22 -0700

    Add session secret key generation

--- signin.py
+++ signin.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, redirect, session
 import json
+import os
 
 app = Flask(__name__)
+
+# I acknowledge the downsides of this line
+app.secret_key = os.urandom(16)
 
 @app.route("/")
 def home():
