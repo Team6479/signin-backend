8ea10172928280f3def86a9053b1c4fbbde15e40
Leo Wilson <leo@leomwilson.com>
2020-01-03 16:00:45 -0700

    Add timezone

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 from datetime import datetime, date, time
+from pytz import timezone
+tz = timezone('US/Arizona')
 
 def fsec(secs: int) -> str:
     hours, remainder = divmod(secs, 3600)
@@ -6,9 +8,9 @@ def fsec(secs: int) -> str:
     return str(hours) + ':' + str(minutes) + ':' + str(seconds)
 
 def ftime_from_dt(ts: int) -> str:
-    t = datetime.fromtimestamp(ts).time()
+    t = datetime.fromtimestamp(ts, tz).time()
     return t.strftime('%H:%M:%S')
 
 def fdate_from_dt(ts: int) -> str:
-    d = datetime.fromtimestamp(ts).date()
+    d = datetime.fromtimestamp(ts, tz).date()
     return d.strftime('%a, %d %b %Y')
