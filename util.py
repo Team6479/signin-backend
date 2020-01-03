d1666195cc8749e4139e3c445eca5cf86b631837
Leo Wilson <leo@leomwilson.com>
2020-01-03 14:40:00 -0700

    Python types are annoying

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ from datetime import datetime, date, time
 def fsec(secs: int) -> str:
     hours, remainder = divmod(secs, 3600)
     minutes, seconds = divmod(remainder, 60)
-    return hours + ':' + minutes + ':' + seconds
+    return str(hours) + ':' + str(minutes) + ':' + str(seconds)
 
 def ftime_from_date(ts: int) -> str:
     t = datetime.fromtimestamp(ts).time()
